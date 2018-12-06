cf5c17de73096b5be96637f6fa36e296c3e693cc
Billy Trend <witrend@microsoft.com>
2018-12-06 11:49:22 -0800

    Started retry wip

--- index.ts
+++ index.ts
@@ -1,64 +1,114 @@
-import * as fsp from "@ts-common/fs"
-import * as https from "@ts-common/https"
-import * as path from "path"
-import * as it from "@ts-common/iterator"
+import * as fsp from "@ts-common/fs";
+import * as it from "@ts-common/iterator";
+import fetch from "node-fetch";
+import * as path from "path";
+import retry from "async-retry";
 
 interface Url {
-    readonly protocol: string
-    readonly path: string
+  readonly protocol: string;
+  readonly path: string;
 }
 
-const protocolSeparator = "://"
+const protocolSeparator = "://";
 
-const toUrlString = (url: Url) => url.protocol + protocolSeparator + url.path
+const toUrlString = (url: Url) => url.protocol + protocolSeparator + url.path;
 
-const urlParse = (dir: string): Url|undefined => {
-    const split = dir.split(protocolSeparator)
-    return split.length === 2 ?
-        {
-            protocol: split[0],
-            path: split[1]
-        } :
-        undefined
-}
+const urlParse = (dir: string): Url | undefined => {
+  const split = dir.split(protocolSeparator);
+  return split.length === 2
+    ? {
+        protocol: split[0],
+        path: split[1]
+      }
+    : undefined;
+};
 
 export const readFile = async (pathStr: string): Promise<string> => {
-    const result = urlParse(pathStr)
-    return result === undefined ?
-        (await fsp.readFile(pathStr)).toString() :
-        await https.getBody(pathStr)
-}
+  const result = urlParse(pathStr);
+  return result === undefined
+    ? (await fsp.readFile(pathStr)).toString()
+    : await getByUrl(pathStr);
+};
+
+const getByUrl = async (url: string) => {
+  return retry<string>(
+    async (bail, retryNr): Promise<string> => {
+      try {
+        const response = await fetch(url);
+        const body = await response.text();
+
+        if (response.status !== 200) {
+          const msg = `StatusCode: "${
+            response.status
+          }", ResponseBody: "${body}."`;
+
+          bail(new Error(msg));
+        }
+
+        return body;
+      } catch (fetchError) {
+        const message = `Request to ${url} failed with error ${fetchError} on
+            retry number ${retryNr}.`;
+
+        throw new Error(message);
+      }
+    },
+    {
+      retries: 3,
+      factor: 1
+    }
+  );
+};
 
 export const pathResolve = (dir: string): string =>
-    urlParse(dir) !== undefined ? dir : path.resolve(dir)
+  urlParse(dir) !== undefined ? dir : path.resolve(dir);
 
 export const pathJoin = (dir: string, value: string): string => {
-    const url = urlParse(dir)
-    return url !== undefined ?
-        toUrlString({
-            protocol: url.protocol,
-            path: it.join(it.concat(url.path.split("/"), [value]), "/")
-        }) :
-        path.join(dir, value)
-}
+  const url = urlParse(dir);
+  return url !== undefined
+    ? toUrlString({
+        protocol: url.protocol,
+        path: it.join(it.concat(url.path.split("/"), [value]), "/")
+      })
+    : path.join(dir, value);
+};
 
 export const exists = async (dir: string): Promise<boolean> => {
-    if (urlParse(dir) !== undefined) {
-        const result = await https.get(dir)
-        return result.statusCode === 200
-    } else {
-        return fsp.exists(dir)
+  if (urlParse(dir) !== undefined) {
+    try {
+      return retry(
+        async (): Promise<boolean> => {
+          const { status } = await fetch(dir);
+
+          if (status !== 200) {
+            const msg = `StatusCode "${status}" for "${dir}"`;
+
+            throw new Error(msg);
+          }
+
+          return true;
+        },
+        {
+          retries: 3,
+          factor: 1
+        }
+      );
+    } catch (e) {
+      return false;
     }
-}
+  } else {
+    return fsp.exists(dir);
+  }
+};
 
 export const pathDirName = (dir: string): string => {
-    const url = urlParse(dir)
-    if (url === undefined) {
-        return path.dirname(dir)
-    }
-    const split = url.path.split("/")
-    return toUrlString({
-        protocol: url.protocol,
-        path: split.length <= 1 ? url.path : it.join(it.dropRight(split), "/")
-    })
-}
+  const url = urlParse(dir);
+  if (url === undefined) {
+    return path.dirname(dir);
+  }
+  const split = url.path.split("/");
+  return toUrlString({
+    protocol: url.protocol,
+    path: split.length <= 1 ? url.path : it.join(it.dropRight(split), "/")
+  });
+};
--- test/test.ts
+++ test/test.ts
@@ -1,76 +1,84 @@
-import * as vfs from "../index"
-import { assert } from "chai"
-import * as path from "path"
+import * as vfs from "../index";
+import { assert } from "chai";
+import * as path from "path";
 
 describe("readFile", () => {
-    it("local", async () => {
-        const result = vfs.readFile("index.ts")
-        assert.notStrictEqual(result, undefined)
-    })
-    it("https", async () => {
-        const result = await vfs.readFile(
-            "https://raw.githubusercontent.com/ts-common/virtual-fs/f836275cd31ecd1fca9c1de9535aea734d642fa6/README.md"
-        )
-        assert.strictEqual(result, "# virtual-fs\nVirtual File System\n")
-    })
-})
+  it("local", async () => {
+    const result = vfs.readFile("index.ts");
+    assert.notStrictEqual(result, undefined);
+  });
+  it("https", async () => {
+    const result = await vfs.readFile(
+      "https://raw.githubusercontent.com/ts-common/virtual-fs/f836275cd31ecd1fca9c1de9535aea734d642fa6/README.md"
+    );
+    assert.strictEqual(result, "# virtual-fs\nVirtual File System\n");
+  });
+});
 
 describe("pathResolve", () => {
-    it("local", () => {
-        const result = vfs.pathResolve("index.ts")
-        assert.strictEqual(result, path.resolve("index.ts"))
-    })
-    it("https", () => {
-        const result = vfs.pathResolve("https://github.com")
-        assert.strictEqual(result, "https://github.com")
-    })
-})
+  it("local", () => {
+    const result = vfs.pathResolve("index.ts");
+    assert.strictEqual(result, path.resolve("index.ts"));
+  });
+  it("https", () => {
+    const result = vfs.pathResolve("https://github.com");
+    assert.strictEqual(result, "https://github.com");
+  });
+});
 
 describe("pathJoin", () => {
-    it("local", () => {
-        const result = vfs.pathJoin("folder", "name")
-        assert.strictEqual(result, "folder" + path.sep + "name")
-    })
-    it("https", () => {
-        const result = vfs.pathJoin("https://github.com", "ts-common")
-        assert.strictEqual(result, "https://github.com/ts-common")
-    })
-})
+  it("local", () => {
+    const result = vfs.pathJoin("folder", "name");
+    assert.strictEqual(result, "folder" + path.sep + "name");
+  });
+  it("https", () => {
+    const result = vfs.pathJoin("https://github.com", "ts-common");
+    assert.strictEqual(result, "https://github.com/ts-common");
+  });
+});
 
 describe("exists", () => {
-    it("local", async () => {
-        const result = await vfs.exists("index.ts")
-        assert.isTrue(result)
-    })
-    it("https", async () => {
-        const result = await vfs.exists(
-            "https://raw.githubusercontent.com/ts-common/virtual-fs/f836275cd31ecd1fca9c1de9535aea734d642fa6/README.md"
-        )
-        assert.isTrue(result)
-    })
-})
+  it("local", async () => {
+    const result = await vfs.exists("index.ts");
+    assert.isTrue(result);
+  });
+  it("https", async () => {
+    const result = await vfs.exists(
+      "https://raw.githubusercontent.com/ts-common/virtual-fs/f836275cd31ecd1fca9c1de9535aea734d642fa6/README.md"
+    );
+    assert.isTrue(result);
+  });
+});
+
+describe("doesn't exist", () => {
+  it("local", async () => {
+    const result = await vfs.exists("thisprobablydoesntexist.ts");
+    assert.isFalse(result);
+  });
+  it("https", async () => {
+    const result = await vfs.exists(
+      "https://raw.githubusercontent.com/Azure/this-repo-doesntexist/exist"
+    );
+    assert.isFalse(result);
+  });
+});
 
 describe("pathDirName", () => {
-    it("local", () => {
-        const result = vfs.pathDirName("folder/index.d.ts")
-        assert.strictEqual(result, "folder")
-    })
-    it("https", () => {
-        const result = vfs.pathDirName(
-            "https://raw.githubusercontent.com/ts-common/virtual-fs/f836275cd31ecd1fca9c1de9535aea734d642fa6/README.md"
-        )
-        assert.strictEqual(
-            result,
-            "https://raw.githubusercontent.com/ts-common/virtual-fs/f836275cd31ecd1fca9c1de9535aea734d642fa6"
-        )
-    })
-    it("domain name", () => {
-        const result = vfs.pathDirName(
-            "https://raw.githubusercontent.com"
-        )
-        assert.strictEqual(
-            result,
-            "https://raw.githubusercontent.com"
-        )
-    })
-})
+  it("local", () => {
+    const result = vfs.pathDirName("folder/index.d.ts");
+    assert.strictEqual(result, "folder");
+  });
+  it("https", () => {
+    const result = vfs.pathDirName(
+      "https://raw.githubusercontent.com/ts-common/virtual-fs/f836275cd31ecd1fca9c1de9535aea734d642fa6/README.md"
+    );
+    assert.strictEqual(
+      result,
+      "https://raw.githubusercontent.com/ts-common/virtual-fs/f836275cd31ecd1fca9c1de9535aea734d642fa6"
+    );
+  });
+  it("domain name", () => {
+    const result = vfs.pathDirName("https://raw.githubusercontent.com");
+    assert.strictEqual(result, "https://raw.githubusercontent.com");
+  });
+});
